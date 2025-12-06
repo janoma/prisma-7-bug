ac1dd5197ba0fa51b2ccf89fc6f518b2a90bc913
Alejandro Mallea <janoma@gmail.com>
2025-12-06 09:44:15 -0800

    improve example

--- index.ts
+++ index.ts
@@ -16,5 +16,9 @@ async function helloBug() {
       },
     },
   });
+  // `tags` should be available here
   const tagsCount = data.map((article) => article.tags.length);
+
+  // `id` should *NOT* be available here
+  const byId = data.map((article) => article.id);
 }
